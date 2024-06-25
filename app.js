f07c185553b9843dbb03a679b7c507344163f477
Chaitu-Tatipamula <chaitutatipamula023@gmail.com>
2024-06-26 00:50:15 +0530

    using an actual get route instead of app.use for / route(home)

--- app.js
+++ app.js
@@ -8,17 +8,20 @@ const uploadFile = require('./routes/upload')
 mongoConnect()
 app.use(express.json());
 
-app.use('/',(req,res)=>{
-    res.send(`<div>
+app.get('/', (req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.send(`
+      <div>
         <h3>/balance/json</h3>
-        <p>use /balance/json if you want to give timestamp(JSON) as body</p>
+        <p>Use /balance/json if you want to give timestamp (JSON) as body</p>
         <h3>/balance?timestamp=2022-09-28%2012:00:00</h3>
-        <p>use /balance?timestamp= if you want to use query parameter</p>
+        <p>Use /balance?timestamp= if you want to use query parameter</p>
         <h3>/upload</h3>
-        <p>use /upload to upload csv file to the Database by creating a readstream</p>
-    </div>`)
-})
-
+        <p>Use /upload to upload CSV file to the database by creating a readstream</p>
+      </div>
+    `);
+  });
+  
 const upload = multer({ dest: 'uploads/' });
 app.use('/upload', uploadFile);
 
