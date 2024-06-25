4a4e49551260a6ea1c1fe2a572723dc93b33fe7d
Chaitu-Tatipamula <chaitutatipamula023@gmail.com>
2024-06-26 00:42:04 +0530

    added some readme at /

---
+++ README.md
@@ -0,0 +1,11 @@
+### /balance/json
+use /balance/json if you want to give timestamp(JSON) as body
+
+### /balance?timestamp=2022-09-28%2012:00:00
+use /balance?timestamp= if you want to use query parameter 
+
+### /upload
+use /upload to upload csv file to the Database by creating a readstream
+
+## Access the API
+[Deployed Api Link](https://some-backend.onrender.com/)
--- app.js
+++ app.js
@@ -8,6 +8,17 @@ const uploadFile = require('./routes/upload')
 mongoConnect()
 app.use(express.json());
 
+app.use('/',(req,res)=>{
+    res.send(`<div>
+        <h3>/balance/json</h3>
+        <p>use /balance/json if you want to give timestamp(JSON) as body</p>
+        <h3>/balance?timestamp=2022-09-28%2012:00:00</h3>
+        <p>use /balance?timestamp= if you want to use query parameter</p>
+        <h3>/upload</h3>
+        <p>use /upload to upload csv file to the Database by creating a readstream</p>
+    </div>`)
+})
+
 const upload = multer({ dest: 'uploads/' });
 app.use('/upload', uploadFile);
 
