73a4d9827258c48abaf17189cc357ebaf388df46
Chaitu-Tatipamula <chaitutatipamula023@gmail.com>
2024-06-26 18:32:28 +0530

    letting user upload csv file through minimal frontend elements

--- app.js
+++ app.js
@@ -4,24 +4,13 @@ const mongoConnect = require('./database/db');
 const multer = require('multer')
 const balance = require('./routes/balance')
 const uploadFile = require('./routes/upload')
+const path = require('path');
 
 mongoConnect()
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.send(`
-      <div>
-        <h3>/balance/json</h3>
-        <p>Use /balance/json if you want to give timestamp (JSON) as body</p>
-        <h3>/balance?timestamp=2022-09-28%2012:00:00</h3>
-        <p>Use /balance?timestamp= if you want to use query parameter</p>
-        <h3>/upload</h3>
-        <p>Use /upload to upload CSV file to the database by creating a readstream</p>
-      </div>
-    `);
-  });
-  
+
 const upload = multer({ dest: 'uploads/' });
 app.use('/upload', uploadFile);
 
